a44746bd89dcfe99c89a15402e1823c671084a40
IlyaKuskov <ilya.kuskov@rubius.com>
2022-05-02 16:11:51 +0700

    gulpfile fixes, webpack+babel config

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 const { src, dest, series, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const browserSync = require("browser-sync").create();
 const del = require("del");
+const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const sourcemaps = require("gulp-sourcemaps");
+const webpackStream = require("webpack-stream");
+const rename = require("gulp-rename");
 
-// Таск компиляции SASS в CSS
 function buildSass() {
   return src("src/scss/**/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass({ includePaths: ["./node_modules"] }).on("error", sass.logError))
+    .pipe(
+      postcss([
+        autoprefixer({
+          grid: true,
+          overrideBrowserslist: ["last 2 versions"],
+        }),
+        cssnano(),
+      ])
+    )
+    .pipe(sourcemaps.write())
     .pipe(dest("src/css"))
     .pipe(dest("dist/css"))
     .pipe(browserSync.stream());
@@ -19,7 +35,16 @@ function buildHtml() {
 
 // Таск копирования статичных файлов
 function copy() {
-  return src(["src/content/**/*.*"], { base: "src" })
+  return src(["src/images/**/*.*"], { base: "src" })
+    .pipe(dest("dist"))
+    .pipe(browserSync.stream());
+}
+
+function buildJs() {
+  return src("src/js/index.js")
+    .pipe(webpackStream(require("./webpack.config")))
+    .pipe(rename("main.min.js"))
+    .pipe(dest("src"))
     .pipe(dest("dist"))
     .pipe(browserSync.stream());
 }
@@ -31,6 +56,7 @@ function cleanDist() {
 
 // Таск отслеживания изменения файлов
 function serve() {
+  watch(["src/js/**/*.js", "!src/js/**/*.min.js"], buildJs);
   watch("src/scss/**/*.scss", buildSass);
   watch("src/**/*.html", buildHtml);
 }
@@ -43,5 +69,8 @@ function createDevServer() {
   });
 }
 
-exports.build = series(cleanDist, buildSass, buildHtml, copy);
-exports.default = series(buildSass, parallel(createDevServer, serve));
+exports.build = series(cleanDist, buildSass, buildHtml, buildJs, copy);
+exports.default = series(
+  [buildSass, buildJs],
+  parallel(createDevServer, serve)
+);
