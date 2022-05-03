27da11195d4597d89a19115e37f8f8db8c95785d
Ilya <kuskovig@gmail.com>
2022-05-03 16:08:18 +0700

    last changes

--- src/js/forms/order-form.js
+++ src/js/forms/order-form.js
@@ -1,11 +1,16 @@
 import ApiService from "../services/api-service";
+import { Fancybox } from "@fancyapps/ui";
+import IMask from 'imask';
 
 export class OrderForm {
   constructor() {
     this.formEl = document.querySelector("#form-modal");
     this.mastersSelect = this.formEl.elements.masterId;
     this.servicesSelect = this.formEl.elements.serviceId;
-    this.loader = document.querySelector('.await-loader');
+    this.loader = document.querySelector('.loader-spinner');
+    this.resultMessage = document.querySelector('.result-message')
+    this.phoneField = this.formEl.elements.phone;
+    this.nameField = this.formEl.elements.name;
 
     this._init();
     this._bindEvents();
@@ -14,18 +19,26 @@ export class OrderForm {
   _init() {
     this._buildMastersSelect();
     this._buildServicesSelect();
+    this.addInputMask();
   }
 
   _bindEvents() {
     this.formEl.addEventListener("submit", async (event) => {
       event.preventDefault();
+      if (!this.validateForm()){
+        return
+      }
       const data = new FormData(this.formEl);
-      this.loader.classList.toggle('visible');
+      this.loader.classList.toggle('loader-spinner_visible');
       let response = await ApiService.createOrder(Object.fromEntries(data));
-      this.loader.classList.toggle('visible');
-      //fancybox close modal
 
-      
+      this.resultMessage.classList.toggle('result-message_visible');
+
+      this.loader.classList.toggle('loader-spinner_visible');
+      setTimeout(() => {
+        Fancybox.close();
+        this.resultMessage.classList.toggle('result-message_visible');
+      }, "3000")
     });
   }
 
@@ -50,4 +63,32 @@ export class OrderForm {
       this.servicesSelect.add(option);
     });
   }
+
+  addInputMask () {
+    let maskOptions = {
+      mask: '+{7}(000)000-00-00'
+    };
+    let mask = IMask(this.phoneField, maskOptions);
+  }
+
+  validateForm() {
+    this.phoneField.classList.toggle('contact-form__input_error', false)
+    this.nameField.classList.toggle('contact-form__input_error', false)
+    let isNameValidated = true;
+    let isPhoneValidated = true;
+
+    if (this.nameField.value == "") {
+      this.nameField.classList.toggle('contact-form__input_error', true)
+      this.nameField.placeholder = "Введите имя";
+      isNameValidated = false
+    }
+
+    if (this.phoneField.value.length < 16 || this.phoneField.value == "") {
+      this.phoneField.classList.toggle('contact-form__input_error', true)
+      this.phoneField.placeholder = "Введите номер телефона";
+      isPhoneValidated = false
+    }
+    return isNameValidated && isPhoneValidated && true;
+  }
+  
 }
--- src/js/main.js
+++ src/js/main.js
@@ -65,65 +65,6 @@ document.querySelectorAll('a[href^="#"]').forEach((a) => {
   });
 });
 
-class TabItem {
-  constructor(link, content) {
-    this.link = link;
-    this.content = content;
-  }
-
-  onClick(callback) {
-    this.link.addEventListener("click", () => callback());
-  }
-
-  activate() {
-    this._toggle(true);
-  }
-
-  deactivate() {
-    this._toggle(false);
-  }
-
-  _toggle(activate) {
-    this.link.classList.toggle("active-item", activate);
-    this.content.classList.toggle("active-table", activate);
-  }
-}
-
-class TabsManager {
-  constructor(tabsElem) {
-    this.tabs = [];
-    this.activeTab = null;
-
-    this.init(tabsElem);
-    this.activateTab(this.tabs[0]);
-  }
-
-  init(tabsElem) {
-    const links = tabsElem.querySelectorAll(".prices__navigation li");
-    const contents = tabsElem.querySelectorAll(".prices__table");
-
-    for (let i = 0; i < links.length; i++) {
-      const tab = new TabItem(links[i], contents[i]);
-      this.tabs.push(tab);
-
-      tab.onClick(() => this.activateTab(tab));
-    }
-  }
-
-  activateTab(tab) {
-    if (this.activeTab) {
-      this.activeTab.deactivate();
-    }
-    this.activeTab = tab;
-    this.activeTab.activate();
-  }
-}
-
-window.onload = function () {
-  const tabsElem = document.querySelector(".prices__content");
-  new TabsManager(tabsElem);
-};
-
 function init() {
   $(".slider").slick({
     draggable: true,
